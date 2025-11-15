63c2125c7544777547107e62dde0265952fd9697
AshokS <ashok.shinde63@gmail.com>
2025-11-15 12:04:42 +0530

    test003.java updated

--- src/test/java/Tutorialsninja/Register/TC_RF_003.java
+++ src/test/java/Tutorialsninja/Register/TC_RF_003.java
@@ -4,24 +4,17 @@ import Tutorialsninja.base.BaseClass;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
-public class TC_RF_004 extends BaseClass {
+public class TC_RF_003 extends BaseClass {
 
     @Test
-    public void duplicateEmail() {
+    public void invalidEmail() {
 
         driver.get("https://tutorialsninja.com/demo/");
 
         driver.findElement(By.xpath("//span[text()='My Account']")).click();
         driver.findElement(By.linkText("Register")).click();
 
-        driver.findElement(By.id("input-firstname")).sendKeys("Test");
-        driver.findElement(By.id("input-lastname")).sendKeys("User");
-        driver.findElement(By.id("input-email")).sendKeys("existing@mail.com"); // should fail
-        driver.findElement(By.id("input-telephone")).sendKeys("9876543210");
-        driver.findElement(By.id("input-password")).sendKeys("test123");
-        driver.findElement(By.id("input-confirm")).sendKeys("test123");
-
-        driver.findElement(By.xpath("//input[@name='agree']")).click();
+        driver.findElement(By.id("input-email")).sendKeys("wrong_email_format");
         driver.findElement(By.xpath("//input[@value='Continue']")).click();
     }
 }
